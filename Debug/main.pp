818fe9ab90154a9ba4ec76680c8b382d26dea46e
tngyn <timngyn@gmail.com>
2014-02-08 17:12:41 -0800

    Revert "Correct voltage display"
    This reverts commit 090ca8587a3354cd0e4f5261995e9711aa887dc1.

--- Debug/main.pp
+++ Debug/main.pp
@@ -1,25 +1,25 @@
 # FIXED
 
 main.obj: ../main.c
-main.obj: C:/Development/Ti/ccsv5/ccs_base/msp430/include/msp430.h
-main.obj: C:/Development/Ti/ccsv5/ccs_base/msp430/include/msp430g2553.h
-main.obj: C:/Development/Ti/ccsv5/ccs_base/msp430/include/in430.h
-main.obj: C:/Development/Ti/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h
+main.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h
+main.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h
 main.obj: ../main.h
 main.obj: ../kent_lcd_display.h
-main.obj: C:/Development/Ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdio.h
-main.obj: C:/Development/Ti/ccsv5/tools/compiler/msp430_4.2.1/include/linkage.h
-main.obj: C:/Development/Ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdarg.h
-main.obj: C:/Development/Ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdlib.h
+main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdio.h
+main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/linkage.h
+main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdarg.h
+main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdlib.h
 
 ../main.c: 
-C:/Development/Ti/ccsv5/ccs_base/msp430/include/msp430.h: 
-C:/Development/Ti/ccsv5/ccs_base/msp430/include/msp430g2553.h: 
-C:/Development/Ti/ccsv5/ccs_base/msp430/include/in430.h: 
-C:/Development/Ti/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h: 
 ../main.h: 
 ../kent_lcd_display.h: 
-C:/Development/Ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdio.h: 
-C:/Development/Ti/ccsv5/tools/compiler/msp430_4.2.1/include/linkage.h: 
-C:/Development/Ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdarg.h: 
-C:/Development/Ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdlib.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdio.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdlib.h: 
